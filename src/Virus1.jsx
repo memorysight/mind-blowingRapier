fc6d816e21d3f333eaa4e3ae672b59c17e920bdf
Daniel Smith <memorysight@gmail.com>
2024-07-23 13:06:41 -0400

    added more animations

--- src/Virus1.jsx
+++ src/Virus1.jsx
@@ -7,7 +7,7 @@ import { AnimationMixer } from 'three';
 
 export function Virus1(){
 
-    const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + "models/jenxxxStanding.glb");
+    const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + "models/JenDancing.glb");
     // const{actions} = useAnimations(gltf.animations, gltf.scene);
 
     // useEffect(()=>{
