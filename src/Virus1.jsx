b66f198f640bfe75f15917abb0446998839cc8a0
Daniel Smith <memorysight@gmail.com>
2024-07-23 13:43:11 -0400

    more animations including arguing

--- src/Virus1.jsx
+++ src/Virus1.jsx
@@ -7,7 +7,7 @@ import { AnimationMixer } from 'three';
 
 export function Virus1(){
 
-    const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + "models/JenHot.glb");
+    const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + "models/JenArguing.glb");
     // const{actions} = useAnimations(gltf.animations, gltf.scene);
 
     // useEffect(()=>{
