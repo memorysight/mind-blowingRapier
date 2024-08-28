eb28a0453658fa97c2866a52c992bdff3d72b78c
Daniel Smith <memorysight@gmail.com>
2024-08-28 18:59:00 -0400

    updated with twerk

--- src/Virus1.jsx
+++ src/Virus1.jsx
@@ -7,7 +7,7 @@ import { AnimationMixer } from 'three';
 
 export function Virus1(){
 
-    const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + "models/ZoeNails.glb");
+    const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + "models/ZoeTwerk.glb");
     // const{actions} = useAnimations(gltf.animations, gltf.scene);
 
     // useEffect(()=>{
